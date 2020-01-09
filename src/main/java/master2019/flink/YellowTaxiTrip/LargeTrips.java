6366b6c991b86ce8744f10f0f2e43c99cc3e8bf9
Carlos Sanchez <carlos.sanchez.marin@alumnos.upm.es>
2020-01-09 11:42:05 +0100

    Updated LargeTrips code to Java8 API

--- src/main/java/master2019/flink/YellowTaxiTrip/LargeTrips.java
+++ src/main/java/master2019/flink/YellowTaxiTrip/LargeTrips.java
@@ -1,7 +1,6 @@
 package master2019.flink.YellowTaxiTrip;
 
-import org.apache.flink.api.common.functions.FilterFunction;
-import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.api.java.tuple.Tuple4;
 import org.apache.flink.api.java.tuple.Tuple5;
@@ -19,55 +18,53 @@ import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.stream.StreamSupport;
 
 /**
  * In this class the Large trips program has to be implemented
  */
 public class LargeTrips {
+
+    final static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    final static DateTimeFormatter dayFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
     public static void main(String[] args) throws Exception {
 
         final ParameterTool params = ParameterTool.fromArgs(args);
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        // env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
 
         final DataStream<String> inputText = env.readTextFile(params.get("input"));
 
         env.getConfig().setGlobalJobParameters(params);
 
-        final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-
         final Long minTime = (19L * 60) + 59;
 
-        SingleOutputStreamOperator<Tuple4<Long, Date, Date, Long>> filterStream = inputText
-                .map(new MapFunction<String, Tuple4<Long, Date, Date, Long>>() {
-                    public Tuple4<Long, Date, Date, Long> map(String in) throws ParseException {
-                        String[] fieldArray = in.split(",");
-                        Tuple4<Long, Date, Date, Long> out = new Tuple4(Long.parseLong(fieldArray[0]),
-                                dateFormat.parse(fieldArray[1]), dateFormat.parse(fieldArray[2]),
-                                stringDatetoSeconds(fieldArray[1], fieldArray[2]));
-                        return out;
-                    }
+        SingleOutputStreamOperator<Tuple4<Long, LocalDateTime, LocalDateTime, Long>> filterStream = inputText
+                .map(in -> {
+                    String[] fieldArray = in.split(",");
+                    Tuple4<Long, LocalDateTime, LocalDateTime, Long> out = new Tuple4(Long.parseLong(fieldArray[0]),
+                            LocalDateTime.parse(fieldArray[1], dateTimeFormatter), LocalDateTime.parse(fieldArray[2], dateTimeFormatter),
+                            stringDatetoSeconds(fieldArray[1], fieldArray[2]));
+                    return out;
                 })
-                .filter(new FilterFunction<Tuple4<Long, Date, Date, Long>>() {
-                    public boolean filter(Tuple4<Long, Date, Date, Long> mapTuple) throws Exception {
-                        return (mapTuple.f3 > minTime);
-                    }
-                });
+                .returns(Types.TUPLE(Types.LONG, Types.LOCAL_DATE_TIME, Types.LOCAL_DATE_TIME, Types.LONG))
+                .filter(mapTuple -> (mapTuple.f3 > minTime));
 
-        KeyedStream<Tuple4<Long, Date, Date, Long>, Tuple> keyedStream = filterStream.
-                assignTimestampsAndWatermarks(new AscendingTimestampExtractor<Tuple4<Long, Date, Date, Long>>() {
+        KeyedStream<Tuple4<Long, LocalDateTime, LocalDateTime, Long>, Tuple> keyedStream = filterStream.
+                assignTimestampsAndWatermarks(new AscendingTimestampExtractor<Tuple4<Long, LocalDateTime, LocalDateTime, Long>>() {
                     @Override
-                    public long extractAscendingTimestamp(Tuple4<Long, Date, Date, Long> tuple) {
-                        return tuple.f1.getTime();
+                    public long extractAscendingTimestamp(Tuple4<Long, LocalDateTime, LocalDateTime, Long> tuple) {
+                        return tuple.f1.toInstant(ZoneId.of("CET").getRules().getOffset(tuple.f1)).toEpochMilli();
                     }
                 })
                 .keyBy(0);
 
-        SingleOutputStreamOperator out = keyedStream.window(TumblingEventTimeWindows.of(Time.hours(3))).apply(new LargeTripsCounter());
+        SingleOutputStreamOperator<Tuple5<Long, String, Long, String, String>> out = keyedStream.window(TumblingEventTimeWindows.of(Time.hours(3))).apply(new LargeTripsCounter());
 
         if (params.has("output")) {
             out.writeAsCsv(params.get("output"), FileSystem.WriteMode.OVERWRITE).setParallelism(1);
@@ -76,27 +73,23 @@ public class LargeTrips {
         env.execute("LargeTrips");
     }
 
-    private static Long stringDatetoSeconds(String start, String finish) throws ParseException {
+    private static Long stringDatetoSeconds(String start, String finish) {
 
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Long startSeconds = dateFormat.parse(start).getTime() / 1000;
-        Long finishSeconds = dateFormat.parse(finish).getTime() / 1000;
+        Long startSeconds = LocalDateTime.parse(start, dateTimeFormatter).toEpochSecond(ZoneOffset.UTC);
+        Long finishSeconds = LocalDateTime.parse(finish, dateTimeFormatter).toEpochSecond(ZoneOffset.UTC);
         return finishSeconds - startSeconds;
     }
 
-    private static class LargeTripsCounter implements WindowFunction<Tuple4<Long, Date, Date, Long>, Tuple5<Long, String, Long, String, String>, Tuple, TimeWindow> {
+    private static class LargeTripsCounter implements WindowFunction<Tuple4<Long, LocalDateTime, LocalDateTime, Long>, Tuple5<Long, String, Long, String, String>, Tuple, TimeWindow> {
 
-        final SimpleDateFormat dayFormat = new SimpleDateFormat("yyyy-MM-dd");
-        final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-
-        public void apply(Tuple tuple, TimeWindow timeWindow, Iterable<Tuple4<Long, Date, Date, Long>> input, Collector<Tuple5<Long, String, Long, String, String>> out) {
+        public void apply(Tuple tuple, TimeWindow timeWindow, Iterable<Tuple4<Long, LocalDateTime, LocalDateTime, Long>> input, Collector<Tuple5<Long, String, Long, String, String>> out) {
             final long numberRecords = StreamSupport.stream(input.spliterator(), false).count();
             if (numberRecords > 4) {
-                Tuple4<Long, Date, Date, Long> first = StreamSupport.stream(input.spliterator(), false).findFirst().get();
+                Tuple4<Long, LocalDateTime, LocalDateTime, Long> first = StreamSupport.stream(input.spliterator(), false).findFirst().get();
                 out.collect(new Tuple5<>(first.f0, dayFormat.format(first.f1),
-                        numberRecords, dateFormat.format(first.f1),
-                        dateFormat.format(StreamSupport.stream(input.spliterator(), false)
-                                .skip(numberRecords-1).findFirst().get().f2)));
+                        numberRecords, dateTimeFormatter.format(first.f1),
+                        dateTimeFormatter.format(StreamSupport.stream(input.spliterator(), false)
+                                .skip(numberRecords - 1).findFirst().get().f2)));
             }
         }
     }
