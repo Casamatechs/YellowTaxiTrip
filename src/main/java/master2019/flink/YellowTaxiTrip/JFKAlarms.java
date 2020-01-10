1333544030315d45979e9680c45cd7513280a346
Carlos Sanchez <carlos.sanchez.marin@alumnos.upm.es>
2020-01-10 19:22:48 +0100

    Removed unused imports

--- src/main/java/master2019/flink/YellowTaxiTrip/JFKAlarms.java
+++ src/main/java/master2019/flink/YellowTaxiTrip/JFKAlarms.java
@@ -1,25 +1,18 @@
 package master2019.flink.YellowTaxiTrip;
 
 import org.apache.flink.api.common.functions.FilterFunction;
-import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.common.typeinfo.Types;
-import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.api.java.tuple.Tuple4;
-import org.apache.flink.api.java.tuple.Tuple5;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
-import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
-import org.apache.flink.util.Collector;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.util.Iterator;
 
 /**
  * In this class the JFK airport trips program has to be implemented.
