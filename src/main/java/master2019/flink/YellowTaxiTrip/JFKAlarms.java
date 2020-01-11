8a4d3e07dabe65c963c3eb5d708d966f56f6c2dd
Carlos Sanchez <casamatechs@gmail.com>
2020-01-11 23:43:25 +0100

    Comments added. Final commit

--- src/main/java/master2019/flink/YellowTaxiTrip/JFKAlarms.java
+++ src/main/java/master2019/flink/YellowTaxiTrip/JFKAlarms.java
@@ -19,7 +19,11 @@ import java.time.format.DateTimeFormatter;
  */
 public class JFKAlarms {
 
-    private final static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    /**
+     * Formatter to convert from the csv date format to Java8 DateTime object. This will be used only to assign
+     * timestamps to the tuples so we avoid the usage of big objects during the execution.
+     */
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     public static void main(String[] args) throws Exception {
 
@@ -30,10 +34,20 @@ public class JFKAlarms {
         env.getConfig().setGlobalJobParameters(params);
 
         env.readTextFile(params.get("input"))
+                /**
+                 * First of all we apply a filter before building the Flink tuples. Doing this we only need the final
+                 * required values when composing the tuples, so we can avoid the creation of thousands of unnecessary
+                 * tuples.
+                 */
                 .filter((FilterFunction<String>) s -> {
                     String[] sp = s.split(",");
                     return sp[5].equals("2") && Long.parseLong(sp[3]) > 1;
                 })
+                /**
+                 * Now we only have the valid tuples to be processed. The map will compose a tuples of 4 elements,
+                 * without parsing the dates to Java DataTime objects, because these are heavier than a String and
+                 * are not necessary for anything but assigning the timestamps, which is done afterwards.
+                 */
                 .map(in -> {
                     String[] fieldArray = in.split(",");
                     return new Tuple4<>(Long.parseLong(fieldArray[0]),
@@ -41,19 +55,41 @@ public class JFKAlarms {
                             fieldArray[2],
                             Long.parseLong(fieldArray[3]));
                 })
+                /**
+                 * Because we are using Java8 lambda expressions, we need to specify to Flink the types of the tuples.
+                 * Otherwise, it'd use Java Object serialization, which is extremely inefficient.
+                 */
                 .returns(Types.TUPLE(Types.LONG, Types.STRING, Types.STRING, Types.LONG))
+                /**
+                 * We assign the timestamps using the start time for each tuple. Since we are using Java8 API, we
+                 * decided not to use the deprecated java.util.Date class. Instead, we used java.time.LocalDateTime
+                 * which provides information about the time zone too. Because the data comes from New York, we used
+                 * that time zone. On this assignment, it doesn't  matter because we don't have data from different
+                 * time zones, but in real applications, this date management is really important.
+                 */
                 .assignTimestampsAndWatermarks(new AscendingTimestampExtractor<Tuple4<Long, String, String, Long>>() {
                     @Override
                     public long extractAscendingTimestamp(Tuple4<Long, String, String, Long> tuple) {
-                        return LocalDateTime.parse(tuple.f1, dateTimeFormatter).atZone(ZoneId.of("CET"))
+                        return LocalDateTime.parse(tuple.f1, dateTimeFormatter).atZone(ZoneId.of("America/New_York"))
                                 .toInstant().toEpochMilli();
                     }
                 })
+                /**
+                 * We group the tuples by VendorId
+                 */
                 .keyBy(0)
+                /**
+                 * We define a tumbling window (size and slide are the same) of 1 hour.
+                 */
                 .window(TumblingEventTimeWindows.of(Time.hours(1)))
+                /**
+                 * Because the input tuple has the output format, we decided to use a reduce function which only
+                 * aggregates the number of passengers. To avoid the creation of unnecessary objects, we reused the
+                 * initial window tuple and updated the required fields.
+                 */
                 .reduce((t1, t2) -> {
-                    t1.setField(t2.f2, 2);
-                    t1.setField(t1.f3 + t2.f3, 3);
+                    t1.setField(t2.f2, 2); // We update the date to get at the end the time the last trip finishes.
+                    t1.setField(t1.f3 + t2.f3, 3); // We sum the passengers to get at the end the total sum.
                     return t1;
                 })
                 .writeAsCsv(params.get("output").concat("/jfkAlarms.csv"), FileSystem.WriteMode.OVERWRITE)
--- src/main/java/master2019/flink/YellowTaxiTrip/LargeTrips.java
+++ src/main/java/master2019/flink/YellowTaxiTrip/LargeTrips.java
@@ -25,7 +25,10 @@ import java.util.stream.StreamSupport;
  */
 public class LargeTrips {
 
-    private final static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    /**
+     * Formatter to convert from the csv date format to Java8 DateTime object.
+     */
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     public static void main(String[] args) throws Exception {
 
@@ -35,26 +38,56 @@ public class LargeTrips {
 
         env.getConfig().setGlobalJobParameters(params);
 
-        final Long minTime = (19L * 60) + 59;
+        /**
+         * The minimum time to go through the filter.
+         */
+        final Long minTime = (20L * 60) - 1;
 
         env.readTextFile(params.get("input"))
+                /**
+                 * First of all we compose the Flink tuples attaching as a field the number of seconds of the trip
+                 * to be able to filter during the next step.
+                 */
                 .map(in -> {
                     String[] fieldArray = in.split(",");
                     return new Tuple4<>(Long.parseLong(fieldArray[0]),
                             fieldArray[1], fieldArray[2],
                             stringDatetoSeconds(fieldArray[1], fieldArray[2]));
                 })
+                /**
+                 * Because we are using Java8 lambda expressions, we need to specify to Flink the types of the tuples.
+                 * Otherwise, it'd use Java Object serialization, which is extremely inefficient.
+                 */
                 .returns(Types.TUPLE(Types.LONG, Types.STRING, Types.STRING, Types.LONG))
+                /**
+                 * Now we apply a filter to remove the trips during less than 20 minutes.
+                 */
                 .filter(mapTuple -> (mapTuple.f3 > minTime))
+                /**
+                 * We assign the timestamps using the start time for each tuple. Since we are using Java8 API, we
+                 * decided not to use the deprecated java.util.Date class. Instead, we used java.time.LocalDateTime
+                 * which provides information about the time zone too. Because the data comes from New York, we used
+                 * that time zone. On this assignment, it doesn't  matter because we don't have data from different
+                 * time zones, but in real applications, this date management is really important.
+                 */
                 .assignTimestampsAndWatermarks(new AscendingTimestampExtractor<Tuple4<Long, String, String, Long>>() {
                     @Override
                     public long extractAscendingTimestamp(Tuple4<Long, String, String, Long> tuple) {
-                        return LocalDateTime.parse(tuple.f1, dateTimeFormatter).atZone(ZoneId.of("CET"))
+                        return LocalDateTime.parse(tuple.f1, dateTimeFormatter).atZone(ZoneId.of("America/New_York"))
                                 .toInstant().toEpochMilli();
                     }
                 })
+                /**
+                 * We group the tuples by VendorId
+                 */
                 .keyBy(0)
+                /**
+                 * We define a tumbling window (size and slide are the same) of 3 hours.
+                 */
                 .window(TumblingEventTimeWindows.of(Time.hours(3)))
+                /**
+                 * We apply a custom WindowFunction which implements the logic to produce the correct output.
+                 */
                 .apply(new LargeTripsCounter())
                 .writeAsCsv(params.get("output").concat("/largeTrips.csv"), FileSystem.WriteMode.OVERWRITE)
                 .setParallelism(1);
@@ -69,13 +102,24 @@ public class LargeTrips {
         return finishSeconds - startSeconds;
     }
 
-    private static class LargeTripsCounter implements WindowFunction<Tuple4<Long, String, String, Long>, Tuple5<Long, String, Long, String, String>, Tuple, TimeWindow> {
+    private static class LargeTripsCounter implements WindowFunction<Tuple4<Long, String, String, Long>,
+            Tuple5<Long, String, Long, String, String>, Tuple, TimeWindow> {
 
-        public void apply(Tuple tuple, TimeWindow timeWindow, Iterable<Tuple4<Long, String, String, Long>> input, Collector<Tuple5<Long, String, Long, String, String>> out) {
+        public void apply(Tuple tuple, TimeWindow timeWindow,
+                          Iterable<Tuple4<Long, String, String, Long>> input,
+                          Collector<Tuple5<Long, String, Long, String, String>> out) {
+            /**
+             * We get the number of tuples in the window in the first place to avoid unnecessary calculations.
+             */
             final long numberRecords = input.spliterator().getExactSizeIfKnown();
             if (numberRecords > 4) {
-                Tuple4<Long, String, String, Long> first = StreamSupport.stream(input.spliterator(), false).findFirst().get();
-                out.collect(new Tuple5<>(first.f0, first.f1.substring(0,10),
+                /**
+                 * Because we are using Java8 API, we decided to use Java Streams, which optimize the use of iterators
+                 * and allow us to skip unnecessary tuples.
+                 */
+                Tuple4<Long, String, String, Long> first = StreamSupport.stream(input.spliterator(), false)
+                        .findFirst().get();
+                out.collect(new Tuple5<>(first.f0, first.f1.substring(0, 10),
                         numberRecords, first.f1,
                         StreamSupport.stream(input.spliterator(), false)
                                 .skip(numberRecords - 1).findFirst().get().f2));
